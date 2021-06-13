58b6b261428a37a77631da672ab2521016aaeac0
ladyinblack <ladyinblack2010rs@gmail.com>
2021-06-13 23:19:36 +0200

    Update to home controller

--- home/home.controller.js
+++ home/home.controller.js
@@ -2,7 +2,55 @@ class HomeCtrl {
   constructor($scope) {
     'ngInject';
 
-    this.name = "AngularJS";
+    // this.name = 'AngularJS';
+
+    $scope.exercises = [
+      {
+        icon:
+          'https://content.codecademy.com/projects/4/move-log/img/pushup.jpg',
+        name: 'Pushup',
+        count: 20
+      },
+      {
+        icon:
+          'https://content.codecademy.com/projects/4/move-log/img/squat.jpg',
+        name: 'Squats',
+        count: 15
+      },
+      {
+        icon:
+          'https://content.codecademy.com/projects/4/move-log/img/pullup.jpg',
+        name: 'Pullups',
+        count: 10
+      },
+      {
+        icon: 'https://content.codecademy.com/projects/4/move-log/img/row.jpg',
+        name: 'Rows',
+        count: 10
+      },
+      {
+        icon:
+          'https://content.codecademy.com/projects/4/move-log/img/lunge.jpg',
+        name: 'Lunges',
+        count: 10
+      },
+      {
+        icon:
+          'https://content.codecademy.com/projects/4/move-log/img/stepup.jpg',
+        name: 'Step Ups',
+        count: 10
+      },
+      {
+        icon:
+          'https://content.codecademy.com/projects/4/move-log/img/situp.jpg',
+        name: 'Sit Ups',
+        count: 15
+      }
+    ];
+
+    this.increase = function(index) {
+      $scope.exercises[index].count += 1;
+    };
   }
 }
 
